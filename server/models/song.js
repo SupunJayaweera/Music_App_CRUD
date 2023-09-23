86a564a2c312084db0018ab11326988cd5808f20
SupunJayaweera <jayaweera_st_e22@engug.ruh.ac.lk>
2023-09-23 22:14:45 +0530

    First Commit

---
+++ server/models/song.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const songSchema = new mongoose.Schema({
+  name: String,
+  artist: String,
+  genre: String,
+  songUrl: String,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+const Song = mongoose.model("Song", songSchema);
+
+module.exports = Song; // Model created
